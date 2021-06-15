03b03aa019b634788c87152ed40a79c08397cd35
eth0 <ethanrobertwheeler@gmail.com>
2021-06-15 14:05:40 -0500

    UPDATE: added auto transparent background

--- lua/nv-autocmds/init.lua
+++ lua/nv-autocmds/init.lua
@@ -24,6 +24,7 @@ utils.define_augroups({
         {'BufWinEnter', '*', 'setlocal formatoptions+=c formatoptions+=r formatoptions+=o'},
         {'BufRead', '*', 'setlocal formatoptions+=c formatoptions+=r formatoptions+=o'},
         {'BufNewFile', '*', 'setlocal formatoptions+=c formatoptions+=r formatoptions+=o'},
+		{'BufWinEnter', '*', 'highlight Normal guibg=none'},
 
         -- {'User', 'GoyoLeave', 'lua require(\'galaxyline\').disable_galaxyline()'},
         -- {'User', 'GoyoEnter', 'lua require(\'galaxyline\').galaxyline_augroup()'},
